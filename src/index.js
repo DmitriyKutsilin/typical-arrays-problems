1fa644cce21e499646ac17970bd1fdb33921fb9b
DmitriyKutsilin <dmitriy.kutsilin@gmail.com>
2020-03-22 00:57:16 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minimum = Math.min.apply(null, array);
+  if (array && array.length) {
+    return minimum;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maximum = Math.max.apply(null, array);
+  if (array && array.length) {
+    return maximum;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let numberSum = 0;
+  if (array && array.length) {
+    for (let i = 0; i < array.length; i++) {
+    numberSum += array[i];
+  }
+    let average = numberSum / array.length;
+    return average;
+  } else {
+    return 0;
+  }
 }
+
